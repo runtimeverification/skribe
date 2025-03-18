625bc7bea884d7da09b1fd553e8225241366a949
Burak Bilge Yalcinkaya <bbyalcinkaya@gmail.com>
2025-03-18 15:43:28 +0300

    add integration tests

--- src/tests/integration/test_integration.py
+++ src/tests/integration/test_integration.py
@@ -1,5 +1,19 @@
-from skribe.hello import hello
+from pathlib import Path
+
+import pytest
+from pyk.kdist import kdist
+from pyk.ktool.krun import _krun
+
+TEST_WAST_DATA = (Path(__file__).parent / 'data' / 'wast').resolve(strict=True)
+TEST_WAST_FILES = TEST_WAST_DATA.glob('*.wast')
+
+DEFINITION_DIR = kdist.get('stylus-semantics.llvm')
 
 
-def test_hello() -> None:
-    assert hello('World') == 'Hello, World!'
+@pytest.mark.parametrize('program', TEST_WAST_FILES, ids=str)
+def test_run_wast(program: Path, tmp_path: Path) -> None:
+    _krun(
+        input_file=program,
+        definition_dir=DEFINITION_DIR,
+        check=True,
+    )
