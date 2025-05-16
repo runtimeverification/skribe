7a67a4f8c2b0f9a5c0fe1cc14bb005b6ab47717a
Burak Bilge Yalcinkaya <bbyalcinkaya@gmail.com>
2025-05-16 14:49:39 +0300

    some helpers for fuzzing

--- src/skribe/utils.py
+++ src/skribe/utils.py
@@ -6,24 +6,32 @@ from typing import TYPE_CHECKING
 from pyk.kast.outer import read_kast_definition
 from pyk.kdist import kdist
 from pyk.konvert import kast_to_kore
+from pyk.kore.manip import substitute_vars
+from pyk.kore.prelude import generated_top
+from pyk.kore.syntax import App
 from pyk.ktool.kompile import DefinitionInfo
 from pyk.ktool.kprove import KProve
 from pyk.ktool.krun import KRun
-from pykwasm import wasm2kast
+from pykwasm.wasm2kast import wasm2kast
 
 if TYPE_CHECKING:
+    from collections.abc import Mapping
     from pathlib import Path
     from subprocess import CompletedProcess
     from typing import Any
 
     from pyk.kast.inner import KInner, KSort
     from pyk.kast.outer import KDefinition
+    from pyk.kore.syntax import EVar, Pattern
     from pyk.ktool.kompile import KompileBackend
+
+
+class SkribeError(RuntimeError): ...
 
 
 def load_wasm(file_path: Path) -> KInner:
     with file_path.open(mode='rb') as f:
-        return wasm2kast.wasm2kast(f, str(file_path))
+        return wasm2kast(f, str(file_path))
 
 
 class SkribeDefinition:
@@ -56,8 +64,6 @@ class SkribeDefinition:
     def krun_with_kast(self, pgm: KInner, sort: KSort | None = None, **kwargs: Any) -> CompletedProcess:
         """Run the semantics on a kast term.
 
-        This will convert the kast term to kore.
-
         Args:
             pgm: The kast term to run
             sort: The target sort of `pgm`. This should normally be `Steps`, but can be `GeneratedTopCell` if kwargs['term'] is True
@@ -71,3 +77,25 @@ class SkribeDefinition:
 
 
 concrete_definition = SkribeDefinition(kdist.get('stylus-semantics.llvm'))
+
+
+def parse_wasm_file(wasm: Path) -> KInner:
+    return wasm2kast(open(wasm, 'rb'))
+
+
+def subst_on_program_cell(template: Pattern, subst: Mapping[EVar, Pattern]) -> Pattern:
+    def skribe_cell(program_cell: Pattern, stylus_cell: Pattern, exit_code_cell: Pattern) -> Pattern:
+        return App("Lbl'-LT-'skribe'-GT-'", args=(program_cell, stylus_cell, exit_code_cell))
+
+    match template:
+        case App(
+            "Lbl'-LT-'generatedTop'-GT-'",
+            args=(
+                App("Lbl'-LT-'skribe'-GT-'", args=(program_cell, stylus_cell, exit_code_cell)),
+                generated_counter_cell,
+            ),
+        ):
+            program_cell_ = substitute_vars(program_cell, subst)
+            return generated_top((skribe_cell(program_cell_, stylus_cell, exit_code_cell), generated_counter_cell))
+
+    raise ValueError(template)
