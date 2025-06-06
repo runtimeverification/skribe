ac4807613365ad1be721c26b68609e7db8ffdf29
Julian Kuners <julian.kuners@gmail.com>
2025-06-06 15:29:11 +0200

    finish nix derivation of `skribe-simulation`

--- nix/skribe/default.nix
+++ nix/skribe/default.nix
@@ -1,55 +1,43 @@
 {
   lib,
+  stdenv,
+  makeWrapper,
   callPackage,
-  nix-gitignore,
 
-  pyproject-nix,
-  pyproject-build-systems,
-  uv2nix,
+  k,
+  which,
 
-  python
-}:
-let
-  pyproject-util = callPackage pyproject-nix.build.util {};
-  pyproject-packages = callPackage pyproject-nix.build.packages {
-    inherit python;
-  };
+  skribe-pyk,
+  rev ? null
+} @ args:
 
-  # load a uv workspace from a workspace root
-  workspace = uv2nix.lib.workspace.loadWorkspace {
-    workspaceRoot = lib.cleanSource (nix-gitignore.gitignoreSourcePure [
-        ../../.gitignore
-        ".github/"
-        "result*"
-        # do not include submodule directories that might be initilized empty or non-existent due to nix/git
-        # otherwise cachix build might not match the version that is requested by `kup`
-        # TODO: for new projects, add your submodule directories that are not required for nix builds here!
-        # e.g., `"/docs/external-computation"` with `external-computation` being a git submodule directory
-        # "/docs/external-computation"
-      ] ../..
-    );
-  };
+stdenv.mkDerivation {
+  pname = "skribe";
+  version = if (rev != null) then rev else "dirty";
+  buildInputs = [
+    skribe-pyk
+    k
+  ];
+  nativeBuildInputs = [ makeWrapper ];
 
-  # create overlay
-  lockFileOverlay = workspace.mkPyprojectOverlay {
-    # prefer "wheel" over "sdist" due to maintance overhead
-    # there is no bundled set of overlays for "sdist" in uv2nix, in contrast to poetry2nix
-    sourcePreference = "wheel";
-  };
+  src = callPackage ../skribe-source { };
 
-  buildSystemsOverlay = import ./build-systems-overlay.nix;
+  dontUseCmakeConfigure = true;
 
-  # construct package set
-  pythonSet = pyproject-packages.overrideScope (lib.composeManyExtensions [
-    # make build tools available by default as these are not necessarily specified in python lock files
-    pyproject-build-systems.overlays.default
-    # include all packages from the python lock file
-    lockFileOverlay
-    # add build system overrides to certain python packages
-    buildSystemsOverlay
-  ]);
-in pyproject-util.mkApplication {
-  # default dependancy group enables no optional dependencies and no dependency-groups
-  venv = pythonSet.mkVirtualEnv "skribe-env" workspace.deps.default;
-  package = pythonSet.skribe;
+  enableParallelBuilding = true;
+
+  buildPhase = ''
+    XDG_CACHE_HOME=$(pwd) ${
+      lib.optionalString
+      (stdenv.isAarch64 && stdenv.isDarwin)
+      "APPLE_SILICON=true"
+    } skribe-kdist -v build 'stylus-semantics.*'
+  '';
+
+  installPhase = ''
+    mkdir -p $out
+    cp -r ./kdist-*/* $out/
+    mkdir -p $out/bin
+    makeWrapper ${skribe-pyk}/bin/skribe-simulation $out/bin/skribe-simulation --prefix PATH : ${lib.makeBinPath [ which k ]} --set KDIST_DIR $out
+  '';
 }
