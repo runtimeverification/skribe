bb29fb1b685849b717ba33fd15776b78ff014d03
Tamás Tóth <tothtamas28@users.noreply.github.com>
2026-05-06 14:30:46 +0200

    Eliminate `kdist` from `skribe-fuzz-rs` build script (#69)
    * Eliminate `kdist` from `skribe-fuzz-rs` build script
    
    * Set Version: 0.1.30
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>

--- skribe-fuzz-rs/build.rs
+++ skribe-fuzz-rs/build.rs
@@ -1,21 +1,8 @@
 use std::env;
-use std::process::Command;
 
 fn main() {
-    let out_dir = env::var("OUT_DIR").unwrap();
-    let kdist_path = format!("{}/kdist", out_dir);
-
-    let _output = Command::new("kdist")
-        .env("KDIST_DIR", &kdist_path)
-        .arg("-v")
-        .arg("build")
-        .arg("stylus-semantics.llvm-library")
-        .output()
-        .expect("Failed to build the kllvm library with kdist");
-
-    let llvm_library_path = format!("{}/stylus-semantics/llvm-library", kdist_path);
-
+    let llvm_library_path = env::var("KLLVM_LIBRARY_PATH")
+        .expect("KLLVM_LIBRARY_PATH must be set to the directory containing interpreter.so");
     println!("cargo:rustc-link-search={}", llvm_library_path);
-
     println!("cargo:rustc-link-lib=dylib:+verbatim=interpreter.so");
 }
