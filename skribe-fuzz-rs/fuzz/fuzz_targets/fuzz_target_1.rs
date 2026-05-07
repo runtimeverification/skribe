cf3862ac2a5a0d91345a8614da3d4e64d392102d
gtrepta <50716988+gtrepta@users.noreply.github.com>
2026-05-07 04:25:53 -0500

    Fuzz specs argument for the fuzzer (#73)
    * feat: Add --fuzz-spec= argument to the fuzz binary
    
    * Set Version: 0.1.35
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>

--- skribe-fuzz-rs/fuzz/fuzz_targets/fuzz_target_1.rs
+++ skribe-fuzz-rs/fuzz/fuzz_targets/fuzz_target_1.rs
@@ -2,10 +2,25 @@
 
 use libfuzzer_sys::fuzz_target;
 
-use skribe_fuzz_rs::{kllvm, make_dv};
+use pico_args::Arguments;
+
+use skribe_fuzz_rs::{kllvm, fuzz_specs_from_json, make_dv};
 
 fuzz_target!( init: {
     kllvm::init();
+    let mut args = Arguments::from_env();
+
+    let fuzz_spec_file: Option<String> = args
+        // You must pass this option as `--fuzz-spec=<specfile>` with the
+        // equals sign, otherwise libfuzzer treats it as a positional argument
+        .opt_value_from_str("--fuzz-spec")
+        .unwrap();
+
+    if let Some(file) = fuzz_spec_file {
+        let contents = std::fs::read_to_string(file).unwrap();
+        let specs = fuzz_specs_from_json(&contents).unwrap();
+        println!("{:?}", specs);
+    }
 },
     |data: &[u8]| {
     let _ = make_dv();
