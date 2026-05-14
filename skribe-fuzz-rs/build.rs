7856b15dfa58b6fe2851e7dac0cf6539e03b2121
Guy Repta <50716988+gtrepta@users.noreply.github.com>
2026-05-14 09:54:08 -0500

    fix: Make the build script platform agnostic

--- skribe-fuzz-rs/build.rs
+++ skribe-fuzz-rs/build.rs
@@ -2,7 +2,7 @@ use std::env;
 
 fn main() {
     let llvm_library_path = env::var("KLLVM_LIBRARY_PATH")
-        .expect("KLLVM_LIBRARY_PATH must be set to the directory containing interpreter.so");
+        .expect("KLLVM_LIBRARY_PATH must be set to the directory containing the interpeter shared object");
     println!("cargo:rustc-link-search={}", llvm_library_path);
-    println!("cargo:rustc-link-lib=dylib:+verbatim=interpreter.so");
+    println!("cargo:rustc-link-lib=dylib:+verbatim=interpreter.{}", std::env::consts::DLL_EXTENSION);
 }
